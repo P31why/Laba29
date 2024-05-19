1db49e74905c76419c84e9528d40e3f18d54973c
P31Dead <P31Dead@LAPETOPE>
2024-05-19 09:10:46 +0300

    Добавьте файлы проекта.

---
+++ Laba29/Phone.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Laba29
+{
+    internal class Phone:INotifyPropertyChanged
+    {
+        private string title;
+        private string company;
+        private int price;
+        public string Title
+        {
+            get=>title;
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        public string Company
+        {
+            get => company;
+            set
+            {
+                company = value;
+                OnPropertyChanged("Company");
+            }
+        }
+        public int Price
+        {
+            get => price;
+            set
+            {
+                price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string prop="")
+        {
+            if (PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
---
+++ Laba29/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace Laba29
+{
+    internal class ViewModel:INotifyPropertyChanged
+    {
+        private Phone phone;
+        public Phone selectedPhone
+        {
+            get => selectedPhone;
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged()
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop="")
+        {
+            if(PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+        {
+
+        }
+
+    }
+}
