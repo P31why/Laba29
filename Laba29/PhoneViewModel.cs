70ac6f2bcd67dc889197e2f03a0d6195b7ee9fc4
P31Dead <P31Dead@LAPETOPE>
2024-05-21 14:58:14 +0300

    Доделана основная модель и начало работы с ICommand

---
+++ Laba29/PhoneViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Laba29
+{
+    class PhoneViewModel
+    {
+        private Phone phone;
+        public PhoneViewModel(Phone p)
+        {
+            phone = p;
+        }
+        public string Title
+        {
+            get => phone.Title;
+            set
+            {
+                phone.Title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        public string Company
+        {
+            get => phone.Company;
+            set
+            {
+                phone.Company = value;
+                OnPropertyChanged("Company");
+            }
+        }
+        public int Price
+        {
+            get => phone.Price;
+            set
+            {
+                phone.Price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop="")
+        {
+            if(PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
--- Laba29/ViewModel.cs
+++ Laba29/ViewModel.cs
@@ -6,14 +6,26 @@ namespace Laba29
 {
     internal class ViewModel:INotifyPropertyChanged
     {
-        private Phone phone;
-        public Phone selectedPhone
+        private Phone selectedPhone;
+        public ObservableCollection<Phone> Phones { get; set; }
+        public ViewModel()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone { Title="iPhone 7", Company="Apple", Price=56000 },
+                new Phone {Title="Galaxy S7 Edge", Company="Samsung", Price =60000 },
+                new Phone {Title="Elite x3", Company="HP", Price=56000 },
+                new Phone {Title="Mi5S", Company="Xiaomi", Price=35000 }
+            };
+        }
+
+        public Phone SelectedPhone
         {
             get => selectedPhone;
             set
             {
                 selectedPhone = value;
-                OnPropertyChanged()
+                OnPropertyChanged();
             }
         }
         public event PropertyChangedEventHandler PropertyChanged;
@@ -23,9 +35,6 @@ namespace Laba29
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(prop));
             }
-        }
-        {
-
         }
 
     }
